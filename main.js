9ac1bc0c61820d90891785711285651e2457dfc9
ehab ghanem <143112212+ehabEG@users.noreply.github.com>
2023-09-04 02:14:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let cont = document.querySelector('.cont');
+function phones(phone){
+    img.src = phone;
+}
+function colors(colorD){
+    cont.style.background = colorD;
+
+}
